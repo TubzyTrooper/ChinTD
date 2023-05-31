7adc56c4416c5649ed78a9e9af39998966a25af6
TubzyTrooper <135119876+TubzyTrooper@users.noreply.github.com>
2023-05-31 12:08:19 +0100

    Update Bezier.cs
    Moved bezier calculations into a fiunction

--- Assets/Scripts/Bezier.cs
+++ Assets/Scripts/Bezier.cs
@@ -8,15 +8,20 @@ public class Bezier : MonoBehaviour
     [SerializeField] private Transform[] m_controlPoints;
     private Vector2 m_gizmoPosition;
 
+    public Vector2 Evaluate(float t)
+    {
+        return Mathf.Pow(1 - t, 3) * m_controlPoints[0].position +
+                3 * Mathf.Pow(1 - t, 2) * t * m_controlPoints[1].position +
+                3 * (1 - t) * Mathf.Pow(t, 2) * m_controlPoints[2].position +
+                Mathf.Pow(t, 3) * m_controlPoints[3].position;
+    }
+
     private void OnDrawGizmos()
     {
         // for i in range(0, 10, 2)
         for (float t = 0; t <= 1; t += 0.05f) 
         {
-            m_gizmoPosition = Mathf.Pow(1 - t, 3) * m_controlPoints[0].position +
-                3 * Mathf.Pow(1 - t, 2) * t * m_controlPoints[1].position +
-                3 * (1 - t) * Mathf.Pow(t, 2) * m_controlPoints[2].position +
-                Mathf.Pow(t, 3) * m_controlPoints[3].position;
+            m_gizmoPosition = Evaluate(t);
 
             Gizmos.DrawSphere(m_gizmoPosition, 0.3f);
         }
