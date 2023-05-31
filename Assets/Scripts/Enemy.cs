dc8c51b4d79dd1b449c9cd8dc52f2734b94f65f8
TubzyTrooper <135119876+TubzyTrooper@users.noreply.github.com>
2023-05-31 11:42:31 +0100

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        hgi4hgi4hi4ghig4hi4gh4gih4gi
     }
 
     // Update is called once per frame
